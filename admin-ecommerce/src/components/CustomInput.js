807c3c2fdff7900422dbaea670351505acfa3222
Ezra Luvaha <mrluvaha@gmail.com>
2023-11-06 22:35:55 +0300

    5th commit

--- admin-ecommerce/src/components/CustomInput.js
+++ admin-ecommerce/src/components/CustomInput.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const CustomInput = (props) => {
-  const { type, label, i_id, i_class, name, val, onCh, onBl } = props;
+  const { type, label, i_id, i_class, name, val, onChng, onBlr } = props;
   return (
     <div className="form-floating mt-3">
       <input
@@ -11,10 +11,10 @@ const CustomInput = (props) => {
         placeholder={label}
         name={name}
         value={val}
-        onChange={onCh}
-        onBlur={onCh}
+        onChange={onChng}
+        onBlur={onBlr}
       />
-      <label htmlFor={i_id}>{label}</label>
+      <label htmlFor={label}>{label}</label>
     </div>
   );
 };
--- admin-ecommerce/src/components/MainLayout.js
+++ admin-ecommerce/src/components/MainLayout.js
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
-import { AiOutlineDashboard, AiOutlineShoppingCart, AiOutlineUser, AiOutlineBgColors } from "react-icons/ai";
-import { IoIosNotifications } from "react-icons/io";
-import { SiBrandfolder } from "react-icons/si";
-import { BiCategory } from "react-icons/bi";
-import { FaClipboardList, FaBlogger } from "react-icons/fa";
-import { ImBlog } from "react-icons/im";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu, Button, theme } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
-
+  AiOutlineDashboard,
+  AiOutlineShoppingCart,
+  AiOutlineUser,
+  AiOutlineBgColors,
+} from "react-icons/ai";
+import { RiCouponLine } from "react-icons/ri";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { ImBlog } from "react-icons/im";
+import { IoIosNotifications } from "react-icons/io";
+import { FaClipboardList, FaBloggerB } from "react-icons/fa";
+import { SiBrandfolder } from "react-icons/si";
+import { BiCategoryAlt } from "react-icons/bi";
+import { Layout, Menu, theme } from "antd";
+import { useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
-const App = () => {
+const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   const navigate = useNavigate();
   return (
-    <Layout>
+    <Layout /* onContextMenu={(e) => e.preventDefault()} */>
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo">
           <h2 className="text-white fs-5 text-center py-3 mb-0">
-            <span className="sm-logo">YW</span>
-            <span className="lg-logo">Yeswa</span>
+            <span className="sm-logo">DC</span>
+            <span className="lg-logo">Dev Corner</span>
           </h2>
         </div>
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['']}
-          onClick={({key}) => {
-            if(key === "signout") {
-
+          defaultSelectedKeys={[""]}
+          onClick={({ key }) => {
+            if (key == "signout") {
             } else {
               navigate(key);
             }
@@ -45,101 +48,118 @@ const App = () => {
             {
               key: "",
               icon: <AiOutlineDashboard className="fs-4" />,
-              label: 'Dashboard',
+              label: "Dashboard",
             },
             {
               key: "customers",
               icon: <AiOutlineUser className="fs-4" />,
-              label: 'Customers',
+              label: "Customers",
             },
             {
-              key: "catalog",
+              key: "Catalog",
               icon: <AiOutlineShoppingCart className="fs-4" />,
-              label: 'Catalog',
+              label: "Catalog",
               children: [
                 {
-                  key: "Product",
-                  icon: <AiOutlineShoppingCart className="fs-4"/>,
-                  label: 'Add Product',
+                  key: "product",
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Add Product",
                 },
                 {
                   key: "list-product",
-                  icon: <AiOutlineShoppingCart className="fs-4"/>,
-                  label: 'Product List',
+                  icon: <AiOutlineShoppingCart className="fs-4" />,
+                  label: "Product List",
                 },
                 {
                   key: "brand",
-                  icon: <SiBrandfolder className="fs-4"/>,
+                  icon: <SiBrandfolder className="fs-4" />,
                   label: "Brand",
                 },
                 {
                   key: "list-brand",
-                  icon: <SiBrandfolder className="fs-4"/>,
-                  label: "Brand List",
+                  icon: <SiBrandfolder className="fs-4" />,
+                  label: "Brand List ",
                 },
                 {
                   key: "category",
-                  icon: <BiCategory className="fs-4"/>,
+                  icon: <BiCategoryAlt className="fs-4" />,
                   label: "Category",
                 },
                 {
                   key: "list-category",
-                  icon: <BiCategory className="fs-4"/>,
+                  icon: <BiCategoryAlt className="fs-4" />,
                   label: "Category List",
                 },
                 {
                   key: "color",
-                  icon: <AiOutlineBgColors className="fs-4"/>,
+                  icon: <AiOutlineBgColors className="fs-4" />,
                   label: "Color",
                 },
                 {
                   key: "list-color",
-                  icon: <AiOutlineBgColors className="fs-4"/>,
+                  icon: <AiOutlineBgColors className="fs-4" />,
                   label: "Color List",
                 },
-              ]
+              ],
             },
             {
               key: "orders",
               icon: <FaClipboardList className="fs-4" />,
-              label: 'Orders',
+              label: "Orders",
+            },
+            {
+              key: "marketing",
+              icon: <RiCouponLine className="fs-4" />,
+              label: "Marketing",
+              children: [
+                {
+                  key: "coupon",
+                  icon: <ImBlog className="fs-4" />,
+                  label: "Add Coupon",
+                },
+                {
+                  key: "coupon-list",
+                  icon: <RiCouponLine className="fs-4" />,
+                  label: "Coupon List",
+                },
+              ],
             },
             {
               key: "blogs",
-              icon: <FaBlogger className="fs-4" />,
-              label: 'Blogs',
+              icon: <FaBloggerB className="fs-4" />,
+              label: "Blogs",
               children: [
                 {
                   key: "blog",
                   icon: <ImBlog className="fs-4" />,
-                  label: 'Add Blog',
+                  label: "Add Blog",
                 },
                 {
                   key: "blog-list",
-                  icon: <FaBlogger className="fs-4" />,
-                  label: 'Blog List',
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog List",
                 },
                 {
                   key: "blog-category",
                   icon: <ImBlog className="fs-4" />,
-                  label: 'Add Blog Category',
+                  label: "Add Blog Category",
                 },
                 {
                   key: "blog-category-list",
-                  icon: <FaBlogger className="fs-4" />,
-                  label: 'Blog Category List',
+                  icon: <FaBloggerB className="fs-4" />,
+                  label: "Blog Category List",
                 },
-              ]
+              ],
             },
             {
               key: "enquiries",
               icon: <FaClipboardList className="fs-4" />,
-              label: 'Enquiries',
+              label: "Enquiries",
             },
           ]}
         />
       </Sider>
-      <Layout>
+      <Layout className="site-layout">
         <Header
           className="d-flex justify-content-between ps-1 pe-5"
           style={{
@@ -147,55 +167,85 @@ const App = () => {
             background: colorBgContainer,
           }}
         >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
           <div className="d-flex gap-4 align-items-center">
             <div className="position-relative">
-              <IoIosNotifications className="fs-4"/>
-              <span className="badge bg-warning rounded-circle p-1 position-absolute">3</span>
+              <IoIosNotifications className="fs-4" />
+              <span className="badge bg-warning rounded-circle p-1 position-absolute">
+                3
+              </span>
             </div>
-            <div
-             className="d-flex gap-3 align-items-center dropdown" >
+
+            <div className="d-flex gap-3 align-items-center dropdown">
               <div>
-                <img width={48} height={64} src="images/ELA.jpg" alt="" />
+                <img
+                  width={32}
+                  height={32}
+                  src="https://stroyka-admin.html.themeforest.scompiler.ru/variants/ltr/images/customers/customer-4-64x64.jpg"
+                  alt=""
+                />
               </div>
               <div
-               role="button" 
-                id="dropdownMenuLink" 
-                data-bs-toggle="dropdown" 
-                aria-expanded="false">
-                <h5 className="mb-0">Ezra</h5>
-                <p className="mb-0">mrluvaha@gmail.com</p>
+                role="button"
+                id="dropdownMenuLink"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <h5 className="mb-0">Navdeep</h5>
+                <p className="mb-0">navdeepdahiya753@gmail.com</p>
               </div>
-              <div
-               className="dropdown-menu"
-               aria-labelledby="dropdownMenuLink">
-                <li><Link className="dropdown-item py-1 mb-1" style={{"height":"auto", lineHeight: "20px"}} to="/">View Profile</Link></li>
-                <li><Link className="dropdown-item py-1 mb-1" style={{"height":"auto", lineHeight: "20px"}} to="/">Sign Out</Link></li>
-               </div>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <li>
+                  <Link
+                    className="dropdown-item py-1 mb-1"
+                    style={{ height: "auto", lineHeight: "20px" }}
+                    to="/"
+                  >
+                    View Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="dropdown-item py-1 mb-1"
+                    style={{ height: "auto", lineHeight: "20px" }}
+                    to="/"
+                  >
+                    Signout
+                  </Link>
+                </li>
+              </div>
             </div>
           </div>
         </Header>
         <Content
           style={{
-            margin: '24px 16px',
+            margin: "24px 16px",
             padding: 24,
             minHeight: 280,
             background: colorBgContainer,
           }}
         >
+          <ToastContainer
+            position="top-right"
+            autoClose={250}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            theme="light"
+          />
           <Outlet />
         </Content>
       </Layout>
     </Layout>
   );
 };
-export default App;
+export default MainLayout;
